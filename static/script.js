f9588cf93a4f57cfd58ebf30365a620210b9ce22
Rohitkumar rathva <rohitrtv95@gmail.com>
2026-04-05 18:51:33 +0530

    Add service worker for caching resources

---
+++ static/script.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'scrub-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;800&display=swap',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
